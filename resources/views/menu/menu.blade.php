25dc22719e4290a704ac240dc66e260cafa9b4d4
ongaikngee@gmail.com <ongaikngee@gmail.com>
2021-04-06 12:18:16 +0800

    'scaffolding'

---
+++ resources/views/menu/menu.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-red-200 p-6 rounded-lg rounded-b-none">
+            menu
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\menuController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menu', [menuController::class, 'index'])->name('menu');
+
